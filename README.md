30230d412018168bd26d5e5ec9b34e2a6867aff3
Adam Hodges <ahodges@shipchain.io>
2019-08-27 09:09:21 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,50 @@
+# Yahoo Pick'em Drag-n-drop UI
+
+A Javascript userscript to add drag and drop UI to [Yahoo Pick'em](https://football.fantasysports.yahoo.com)
+
+![Drag-n-drop Preview](img/pickem-dnd.gif)
+
+## Using the script as a JavaScript bookmark
+
+Fast and lightweight way to run the script.
+
+1. Go to the bookmark menu of your browser and add a new bookmark with the title of your choice.
+
+2. Copy the following snippet and paste it into the URL-Field: `javascript:(function(){document.body.appendChild(document.createElement('script')).src='https://ajhodges.github.io/pickem-dnd/pickem-dnd.user.js';})();`
+
+3. Save the Bookmark.
+
+4. From now on, you can just click on that bookmark when you have the Pick'em tab open to enable the script.
+
+## Installing the script using Tampermonkey
+
+Installing the userscript via Tampermonkey will automatically run it everytime you visit the Pick'em site.
+
+1. Install the [Tampermonkey extension](https://www.tampermonkey.net/), supported for all major browsers.
+
+2. Click this link to navigate to the script URL: https://ajhodges.github.io/pickem-dnd/pickem-dnd.user.js
+
+3. Tampermonkey will detect the userscript and will open a new tab. Click on `Install to Tampermonkey` and click Ok.
+
+4. Refresh the Pick'em site and the new UI should be activated.
+
+## Run the script via the console (no extensions needed)
+
+If you don't want or can't install the previously mentioned browser extension, one possibility is to run the script via the developer console. However, you will need to rerun the script every time you refresh the stream.
+
+1. On the Pick'em page, open your broser's developer console.
+    * On Firefox, press `Ctrl` + `Shift` + `K`
+    * On Chrome, press `Ctrl` + `Shift` + `J`
+    * On Safari, press `Ctrl` + `Alt` + `I`
+    * On IE9+, press `F12`
+    * On Opera, press `Ctrl` + `Shift` + `I`
+    * If you are having trouble opening your console, try reading the in depth explanation [here](http://webmasters.stackexchange.com/questions/8525/how-to-open-the-javascript-console-in-different-browsers)
+
+2. Copy the following snippet and paste it into the developer console on the Pick'em page: `javascript:(function(){document.body.appendChild(document.createElement('script')).src='https://ajhodges.github.io/pickem-dnd/pickem-dnd.user.js';})();`
+
+3. Press `Enter` to run the code.
+
+## Update Log
+
+Version 1.0 (2019/08/27)
+- Initial release with basic dnd functionality
---
+++ pickem-dnd.user.js
@@ -0,0 +1,97 @@
+// ==UserScript==
+// @name         Yahoo Pick'em Drag n' Drop
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  No more awful confidence points UI!
+// @author       ajhodges
+// @match        https://football.fantasysports.yahoo.com/*
+// @grant        none
+// @require http://code.jquery.com/jquery-3.4.1.min.js
+// @require https://code.jquery.com/ui/1.12.1/jquery-ui.min.js
+// ==/UserScript==
+
+/* jshint esversion: 6 */
+/* globals $:false */
+
+function addGlobalStyle(css) {
+    var head;
+    var style;
+    head = document.getElementsByTagName("head")[0];
+    if (!head) { return; }
+    style = document.createElement("style");
+    style.type = "text/css";
+    style.innerHTML = css;
+    head.appendChild(style);
+}
+
+(function() {
+    "use strict";
+
+    // Add style for placeholder (hover element)
+    addGlobalStyle(`.dnd-placeholder {
+    height:                     55px;
+    border:                     2px dashed red;
+    }`);
+
+    // Add handle for dragging
+    addGlobalStyle(`.conf-score {
+    cursor: grab;
+    }
+    `);
+    addGlobalStyle(`.conf-score::after {
+    content: '\\E00B';
+    font-family: "yahoo";
+    }`);
+
+    var numGames = $("#ysf-picks-table tbody tr").length;
+
+    $("#ysf-picks-table tbody").sortable({
+        axis: "y",
+        containment: $("#ysf-picks-table"),
+        cursor: "grabbing",
+        handle: ".conf-score",
+        opacity: 0.8,
+        placeholder: "dnd-placeholder",
+        revert: 100,
+        create: function(e, ui) {
+            // Sort initially by confidence
+            var rows = $("#ysf-picks-table tbody tr").get();
+
+            rows.sort(function(row1, row2){
+                // Sort descending
+                var val1 = parseInt($(row1).find("select").val());
+                var val2 = parseInt($(row2).find("select").val());
+                return val2 - val1;
+            });
+
+            $(rows).each(function(index){
+                $(this).find("select").val(numGames - index); // Assign select value if blank
+                $("#ysf-picks-table tbody").append(rows[index]);
+            });
+
+            // Set confidence dropdowns to read-only
+            $("#ysf-picks-table tbody select").css("pointer-events","none");
+        },
+        start : function(event, ui) {
+            ui.item.data("start-pos", ui.item.index()+1);
+        },
+        change: function(e, ui) {
+            // Reassign confidence points based on dragging
+            var startPos = ui.item.data("start-pos");
+
+            var correction = (startPos <= ui.placeholder.index()) ? 0 : 1;
+
+            // Resort all other games/points
+            ui.item.parent().find("tr:not(.ui-sortable-helper)").each(function(idx, el){
+                var index = $(el).index();
+                if ( ( index+1 >= startPos && correction === 0) || (index+1 <= startPos && correction === 1 ) )
+                {
+                    $(this).find("select").val(numGames + 1 - (index + correction));
+                }
+            });
+
+            // Get new value for 'floating' game
+            ui.item.find("select").val(numGames + 1 - (ui.placeholder.index() + correction));
+        }
+    });
+})();
