152138637de8dbe44093ae4858af59d413e26230
Adam Hodges <ahodges@shipchain.io>
2019-09-18 22:52:50 -0400

    Version bump

--- pickem-dnd.user.js
+++ pickem-dnd.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Yahoo Pick'em Drag n' Drop
 // @namespace    http://tampermonkey.net/
-// @version      1.2
+// @version      1.3
 // @description  No more awful confidence points UI!
 // @author       ajhodges
 // @match        https://football.fantasysports.yahoo.com/*
